23cd4c87af014b486c7cba59f71c1c7cb506754a
theantidoak <tcdoak@gmail.com>
2024-01-29 21:29:55 +0100

    init commit

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+//Challenge
+
+router.get('/cool/', function(req, res, next) {
+  res.render('cool', { title: 'Challenge', message: 'You\'re so cool' });
+});
+
+
+module.exports = router;
